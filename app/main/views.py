b68f137e1084f5939c0f100899f6aad45683d883
Robert Maina <robert.maina@student.moringaschool.com>
2021-08-24 06:56:13 +0300

    added about.html

--- app/main/views.py
+++ app/main/views.py
@@ -6,3 +6,8 @@ def index():
 
     title= 'Welcome to Myner Blog'
     return render_template('index.html', title = title)
+
+@main.route('/about')
+def about():
+    tittle = 'About Myners Pitches'
+    return render_template('about.html', tittle = tittle)
